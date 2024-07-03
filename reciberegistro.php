fdd2c70df3a0fc44f32752059721900120d51c36
melisaphp <melisaromina.c00@gmail.com>
2024-07-03 19:05:17 -0300

    bdd-consulta

--- reciberegistro.php
+++ reciberegistro.php
@@ -41,8 +41,69 @@ $consultas = mysqli_query($conexion, "SELECT * FROM usuarios");
 if ($consultas) {
     // Mostrar los resultados
     while ($lista = mysqli_fetch_assoc($consultas)) {
-        print_r ($lista);
-        echo "<br>";
+        // print_r ($lista);
+        // echo "<br>";
+
+        ?>
+
+
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="styles.css" type="text/css">
+            <link rel="stylesheet" href="styles-registro.css" type="text/css">
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
+            <script src="https://kit.fontawesome.com/d7f8f62978.js" crossorigin="anonymous"></script>
+            <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+            <title>CAC-MOVIES</title>
+        </head>
+        <body>
+            <header class="nav-princ">
+                <nav>
+                    <a href="index.html" id="logo-princ" class="animate__swing">
+                        <i class="fa-solid fa-film" style="color: #ffffff;"></i>
+                        <p>CAC-Movies</p>
+                    </a>
+                </nav>
+            </header>
+            <main id="registro-princ" class="section-buscador">
+                
+                  
+        
+        
+              <h1>Usuario registrado correctamente</h1>
+        
+                  <article class="animate__zoomIn" id="form_reg">
+        
+                    <h2>Nombre "<?php echo $lista?>"</h2>
+
+                    <a href="index.html"><button class="boton-sec">Volver</button></a>
+        
+                  </article>
+        
+        
+            </main>
+            <footer class="nav-princ">
+                <ul>
+                    <a href=""><li>Términos y condiciones</li></a>
+                    <a href=""><li>Preguntas frecuentes</li></a>
+                    <a href=""><li>Ayuda</li></a>
+                    <a href="administrar.html"><li>Administrar peliculas</li></a>
+                </ul>
+            </footer>
+        
+        
+        </body>
+        </html>
+        
+
+        <?php
     }
 } else {
     echo "Error en la consulta: " . mysqli_error($conexion);
@@ -55,7 +116,3 @@ var_dump($consultas);
 
 // Cerrar conexión a la base de datos
 mysqli_close($conexion);
-?>
-
-
-?>
